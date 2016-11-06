126cae4996c04de2f0f542b8b5918e961786f82b
Dominick <dominickmalzone@aol.com>
2016-11-06 12:28:22 -0800

    made it easier to read

--- scrape.py
+++ scrape.py
@@ -2,16 +2,11 @@ from bs4 import BeautifulSoup
 import urllib2
 import csv
 
-# WHAT TOPICS DO YOU WANT TO SEARCH FOR ?
+
 topic_to_scrape = raw_input("Keyword to search to search for: ")
-
-#= 'technology' # one only
-
-
-url = "http://api.meetup.com/2/groups.xml/?&topic=" + topic_to_scrape +"&order=members&key=3137117152a38291415976a3fb23"
+url = "http://api.meetup.com/2/groups.xml/?&topic=" + topic_to_scrape +"&order=members&key=f4243746215f7de1a57433b3e56"
 content = urllib2.urlopen(url).read()
 soup = BeautifulSoup(content,'xml')
-
 
 #choose output file name
 resultFile = open(topic_to_scrape + ".csv", 'wb')
@@ -24,32 +19,25 @@ urlname = soup.find_all("urlname")
 organizer = soup.find_all("organizer")
 city = soup.find_all("city")
 
-#Write Header
-
 RESULT = ['Number of Members','Group Name','Location','Meetup Link','Organizer ID','Organizer Image']
 wr.writerow(RESULT)
 
 for i in range(length):
-
 	print "Getting Data: " + str(i)
-
 	#Getting the members
 	temp_members = members[i].contents  						
 	for child in (temp_members):
 		child_members = child.encode('utf-8')
-
 	#Getting the url name
 	temp_url = urlname[i].contents
 	for child in (temp_url):
 		child_url = child.encode('utf-8')
 		child_link = 'https://www.meetup.com/' + child_url + '/'
-
 	#Getting the city
 	temp_city = city[i].contents
 	for tag in soup.find_all("organizer"):
 		for child in (temp_city):
 			child_city = child.encode('utf-8')
-
 	#Getting the organizer and the Organizer ID
 	temp_organizer = organizer[i].contents
 	organizer_contents = temp_organizer
@@ -64,9 +52,6 @@ for i in range(length):
 		child_str1 = child_str.replace('</photo_link>','')
 		child_name = child_str1.encode('utf-8')
 
-
-	#printing to excel row
 	excelList = [child_members,child_url,child_city,child_link,child_id,child_name]
 	wr.writerow(excelList)
-
 
